683ea87f007e8bb49446a060e3ffb6657ff0407d
james <jleo56291@gmail.com>
2024-10-16 11:05:50 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,75 @@
+"use client"
 import Image from "next/image";
+import {Link as ScrollLink, Element} from "react-scroll";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div  className="overflow-clip inset-0 -z-10 h-screen w-full bg-[#fafafa] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+    
+    <Element 
+    name="top"
+    className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 xl:w-4/5 2xl:w-[68%] bg-white flex-items-center justify-between py-6 px-4 md:px-8 mx-6">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <Link href={"/"}>
+    <Image 
+    src={'/logo/logo.webp'}
+    alt="Logo"
+    width={1000}
+    height={1000}
+    className="w-28"
+
+    />
+    </Link>
+    <div className="absolute right-1/2 translate-x-1/2 transform">
+    <div className="hidden md:flex gap-x-10  items-center text-gray-700 font-medium text-lg cursor-pointer ">
+
+      <Link href={'/showcase'} 
+      className="hover:text-blue-500">
+        Showcase
+      </Link>
+      
+      <ScrollLink to={'/services'}
+      className="hover:text-blue-500">
+        Services
+
+      </ScrollLink>
+
+      <ScrollLink to="process" smooth={true}
+      className="hover:text-blue-500">
+        Process
+      </ScrollLink>
+
+      <ScrollLink to="guarentess" smooth={true}
+      className="hover:text-blue-500">
+        Guarentees
+
+      </ScrollLink>
     </div>
+    </div>
+
+    <div className="flex items-center gap-x-4">
+    <a href="tel:+25414178292" className="hidden lg:flex">
+            <button className="px-4 py-2 rounded-md flex items-center gap-x-3">
+              (714)-178-292
+            </button>
+          </a>
+
+
+    </div>
+
+
+
+
+
+
+
+
+    </Element>
+    
+      
+
+    </div>
+   
   );
 }
